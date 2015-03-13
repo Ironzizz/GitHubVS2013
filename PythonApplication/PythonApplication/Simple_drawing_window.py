ab3f88f216e9aa7c77b65a35330f3671a023b619
Peeranut Chindaononda <PCXD@me.com>
2015-03-13 13:38:53 +0700

    Create Simple_drawing_window classes

---
+++ PythonApplication/PythonApplication/Simple_drawing_window.py
@@ -0,0 +1,7 @@
+from PySide.QtCore import *
+from PySide.QtGui import *
+from Simple_drawing_window2 import *
+
+class Simple_drawing_window(Simple_drawing_window2):
+    def __init__(self):
+        pass
---
+++ PythonApplication/PythonApplication/Simple_drawing_window2.py
@@ -0,0 +1,6 @@
+from PySide.QtCore import *
+from PySide.QtGui import *
+
+class Simple_drawing_window2(QWidget):
+    def paintEvent(self, e):
+        pass
